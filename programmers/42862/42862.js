91b80e4f8404aed20237688e37296e15a45f48fd
Younho Choo <yo+github@younho9.com>
2023-06-19 13:57:24 +0900

    :hankey: programmers 42862

---
+++ programmers/42862/42862.js
@@ -0,0 +1,22 @@
+Set.prototype.difference = function (setB) {
+	let difference = new Set(this);
+	for (let elem of setB) {
+		difference.delete(elem);
+	}
+	return difference;
+};
+
+export function solution(n, lost, reserve) {
+	const reserveSet = new Set(reserve).difference(new Set(lost));
+	const lostSet = new Set(lost).difference(new Set(reserve));
+
+	reserveSet.forEach((i) => {
+		if (lostSet.has(i - 1)) {
+			lostSet.delete(i - 1);
+		} else if (lostSet.has(i + 1)) {
+			lostSet.delete(i + 1);
+		}
+	});
+
+	return n - lostSet.size;
+}
---
+++ programmers/42862/42862.test.js
@@ -0,0 +1,40 @@
+import {describe, it, expect} from 'vitest';
+import {solution} from './42862.js';
+
+describe('42862', () => {
+	it('test 1', () => {
+		const n = 5;
+		const lost = [2, 4];
+		const reserve = [1, 3, 5];
+		const result = 5;
+
+		expect(solution(n, lost, reserve)).toEqual(result);
+	});
+
+	it('test 2', () => {
+		const n = 5;
+		const lost = [2, 4];
+		const reserve = [3];
+		const result = 4;
+
+		expect(solution(n, lost, reserve)).toEqual(result);
+	});
+
+	it('test 3', () => {
+		const n = 3;
+		const lost = [3];
+		const reserve = [1];
+		const result = 2;
+
+		expect(solution(n, lost, reserve)).toEqual(result);
+	});
+
+	it('test 4', () => {
+		const n = 3;
+		const lost = [3];
+		const reserve = [3];
+		const result = 3;
+
+		expect(solution(n, lost, reserve)).toEqual(result);
+	});
+});
